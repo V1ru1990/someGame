37861b9499af8cea38347204b5aa660cd1b232a5
V1ruS <grunichev.sasha20011228@yandex.ru>
2025-06-12 14:39:13 +0300

    Triange ya hs

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,46 @@
 int glfw_windowSizeX = 640;
 int glfw_windowSizeY = 480;
 
+// координаты вертексов дл€ треугольника в виде массива point[]
+GLfloat point[] = {     
+     0.0f,  0.5f,  0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+//rgb дл€ кадлого соответственного вертекса в виде массива colors[]
+GLfloat colors[]{
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+
+//glsl начало участка
+// передаем цвет вершины дальше по конвейеру и устанавливаем позицию вершины дл€ растеризации.
+const char* vertex_shader =
+    "#version 460\n"
+    "layout(location = 0) in vec3 vertex_position;"
+    "layout(location = 1) in vec3 vertex_color;"
+    "out vec3 color;"
+    "void main() {"
+    "   color = vertex_color;"
+    "   gl_position = vec4(vertex_position, 1.0);"
+    "}";
+
+
+/*берем интерполированный (плавно измен€ющийс€ между вершинами) цвет
+устанавливаем его как цвет фрагмента с альфа-каналом = 1.0 (без прозрачности)*/
+const char* fragment_shader =
+    "#version 460\n"
+    "in vec3 color;"
+    "out vec4 frag_color;"
+    "void main() {"
+    "   frag_color = vec4(color, 1.0);"
+    "}";
+
+//glsl конец участка
+
 void glfw_WindowSizeCallback(GLFWwindow* pWindow, int width, int height) {
     glfw_windowSizeX = width;
     glfw_windowSizeY = height;
@@ -27,9 +67,6 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    const int WIDTH = 640;
-    const int HEIGHT = 480;
-        
 
     /* Create a windowed mode window and its OpenGL context */
     GLFWwindow* pWindow = glfwCreateWindow(glfw_windowSizeX, glfw_windowSizeY, "Some Game", nullptr, nullptr);
@@ -55,12 +92,56 @@ int main(void)
     std::cout << glGetString(GL_RENDERER) << std::endl;
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    glClearColor(1, 0, 0, 1);
+    glClearColor(1, 1, 1, 1);
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vs);
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+
+    GLuint vertexArrayObject = 0;
+    glGenVertexArrays(1, &vertexArrayObject);
+    glBindVertexArray(vertexArrayObject);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glad_glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glad_glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(shader_program);
+        glBindVertexArray(vertexArrayObject);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
